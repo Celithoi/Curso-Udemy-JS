d7930fc819c99952839f579a678f751cbc4bb975
Marcelo Henrique Rocha Cardoso <100224714+Celithoi@users.noreply.github.com>
2022-08-23 14:56:03 -0300

    exercicios
    continuando no aprendizado do basico

---
+++ aula 6/modelo/assets/js/main.js
@@ -0,0 +1,13 @@
+//function verifica(a,b){
+//if(a > b) return x;
+//return y;
+
+//##############################
+
+////return x > y ? x : y
+//}
+
+//ou
+const max = (x,y) => x > y ? x : y;
+
+console.log(max(2,10))
