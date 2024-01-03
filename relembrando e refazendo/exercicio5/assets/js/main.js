8ccb3b6504747cbc4259286228c391cd78919642
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2024-01-03 19:23:02 -0300

    criando lista de tarefas
    Lista de tarefas salvando dados em JSON no navegador

--- relembrando e refazendo/exercicio5/assets/js/main.js
+++ relembrando e refazendo/exercicio5/assets/js/main.js
@@ -2,15 +2,41 @@ const inputTarefa = document.querySelector('.input-tarefa')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefas = document.querySelector('.tarefas')
 
+function recuperarTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+    for (let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+recuperarTarefasSalvas();
+
 function criaLi(){
     const li = document.createElement('li');
     return li;
 }
 
+function salvarTarefas(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const arrayTarefas = [];
+
+    for(let tarefa of liTarefas){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar', "").trim();
+        arrayTarefas.push(tarefaTexto);
+        
+    }
+    // Salvando em JSON
+    const tarefasJSON = JSON.stringify(arrayTarefas);
+    localStorage.setItem('tarefas',tarefasJSON);
+    
+};
+
 function criaBotaoApagar(li){
     li.innerText += ' ';
-    const botaoApagar = createElement('button')
+    const botaoApagar = document.createElement('button');
     botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
     li.appendChild(botaoApagar);
 }
 
@@ -24,6 +50,8 @@ function criaTarefa(textoInput){
     li.innerText = textoInput;
     tarefas.appendChild(li);
     limpaImput();
+    criaBotaoApagar(li);
+    salvarTarefas();
 }
 
 inputTarefa.addEventListener('keypress', function(e){
@@ -38,4 +66,12 @@ btnTarefa.addEventListener('click',function(){
     //verificação se há dados de entrada, caso negativo sai da função
     if(!inputTarefa.value) return;
         criaTarefa(inputTarefa.value);
+});
+
+document.addEventListener('click',function(e){
+    const el = e.target;
+    if (el.classList.contains('apagar')){
+        el.parentElement.remove();
+        salvarTarefas();
+    }
 });
