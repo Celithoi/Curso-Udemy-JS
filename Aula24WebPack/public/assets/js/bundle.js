c6aa0df1d8d6bf7fe3d10dc2331ece7b7b984839
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2024-01-19 09:55:46 -0300

    modulos em js
    modulos em js

---
+++ Aula24WebPack/public/assets/js/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('teste');
+alert('ola mundo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
