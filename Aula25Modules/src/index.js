89d79500c8e4fc0bc813796536bedfa1e33c9413
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2024-01-19 14:37:47 -0300

    aula de modulos
    aula de modulos

---
+++ Aula25Modules/src/index.js
@@ -0,0 +1,8 @@
+import { nome , sobrenome , idade } from './modulo1';
+import soma from './modulo1';
+
+//ou
+//import Soma,{ nome , sobrenome , idade } from './modulo1';
+
+console.log(nome,sobrenome,idade);
+console.log(soma(5,5));
