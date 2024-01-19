f12f084b25f60e19c5892ff53897e89c2b778cb0
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2024-01-19 18:04:47 -0300

    fazendo correções em navegadores antigos
    fazendo correções em navegadores antigos

---
+++ Aula28/src/main.js
@@ -0,0 +1,7 @@
+import './assets/css/style.css';
+// estes dois imports abaixos faz funcionar o js em navegadores antigos
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import executa from './modules/promises';
+
+executa();
