bc15b25bd74824f0807f420a2b2a951f68df7f74
Marcelo Henrique Rocha Cardoso <mhrcardoso@gmail.com>
2024-02-06 00:13:27 -0300

    modificando a pagina de login
    modificando a pagina de login

--- projetoAgenda/src/models/loginModel.js
+++ projetoAgenda/src/models/loginModel.js
@@ -16,6 +16,25 @@ class Login {
        this.user = null; 
     }
 
+    async login(){
+        this.valida();
+        if(this.errors.length > 0) return;
+        // usuario recebe uma tentativa de login procurando um email no BD
+        this.user = await LoginModel.findOne({email: this.body.email});
+
+        if(!this.user) {
+            this.errors.push('Usuário não existe');
+            return;
+        }
+    
+        if(bcryptjs.compareSync(this.body.password , this.user.password)){
+            this.errors.push('Senha inválida')
+            this.user = null;
+            return;
+        }
+
+    }
+
     async register(){
         this.valida();
         if(this.errors.length > 0) return;
@@ -24,17 +43,15 @@ class Login {
         if(this.errors.length >0) return;
         const salt = bcryptjs.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt)
-
-        try{
-            this.user = await LoginModel.create(this.body)
-        }catch(e){
-            console.log(e);
-        }
+        
+        this.user = await LoginModel.create(this.body)
+       
     }
 
     async userExists(){
-        const user = await LoginModel.findOne({email: this.body.email});
-        if (user) this.errors.push('Email ja cadastrado.')
+        this.user = await LoginModel.findOne({email: this.body.email});
+        if (this.user) this.errors.push('Email ja cadastrado.')
+        
     }
 
     valida(){
